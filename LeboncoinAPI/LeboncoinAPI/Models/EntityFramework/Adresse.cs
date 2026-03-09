a0ff476ece3ba659f32975d7552b3e72ccbb8ca8
BazeOnigiri <timkiehl@icloud.com>
2026-03-09 16:27:36 +0100

    feat(models)! : ajouter la plupart des models et le dbcontext
    +adresse
    +annonce
    +avis
    +incident
    +message
    +particulier
    +professionnel
    +reservation
    +utilisateur
    +ville
    
    +leboncoindbcontext

---
+++ LeboncoinAPI/LeboncoinAPI/Models/EntityFramework/Adresse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeboncoinAPI.Models.EntityFramework;
+
+[Table("adresse")]
+public partial class Adresse
+{
+    [Key]
+    [Column("idadresse")]
+    public int AdresseId { get; set; }
+
+    [Column("idville")]
+    public int VilleId { get; set; }
+
+    [Column("numerorue")]
+    public int? NumeroRue { get; set; }
+
+    [Column("nomrue")]
+    [StringLength(39)]
+    public string? NomRue { get; set; }
+
+    // --- Navigation Properties ---
+
+    [ForeignKey(nameof(VilleId))]
+    public virtual Ville VilleAdresse { get; set; } = null!;
+}
