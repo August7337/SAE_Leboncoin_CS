9e9ce97a8e682c89b5e91018462bea591a03d9ae
scassen <sacha.cassen@etu.univ-smb.fr>
2026-03-18 16:22:23 +0000

    fix: index.js

--- LeboncoinClient/src/router/index.js
+++ LeboncoinClient/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { authState } from '@/auth' // Importé en haut, c'est plus propre
+import { authState } from '@/auth' 
 
 const routes = [
   {
@@ -29,7 +29,13 @@ const routes = [
     name: 'professionnel',
     component: () => import('../views/auth/ProfessionnelView.vue')
   },
+
   // --- Annonces ---
+  {
+    path: '/search',
+    name: 'search',
+    component: () => import('../views/annonces/AnnonceSearchView.vue')
+  },
   {
     path: '/annonce/:id',
     name: 'annonce-detail',
@@ -38,46 +44,17 @@ const routes = [
   {
     path: '/create-annonce',
     name: 'create-annonce',
-    component: () => import('../views/annonces/CreateAnnonceView.vue')
-    path: '/create-annonce',
-    name: 'create-annonce',
     component: () => import('../views/annonces/CreateAnnonceView.vue'),
-    meta: { requiresAuth: true } // Ajouté pour protéger la route
+    meta: { requiresAuth: true }
   },
-  {
-    path: '/edit-annonce/:id',
-    name: 'edit-annonce',
-    component: () => import('../views/annonces/EditAnnonceView.vue')
-  },
-  {
   {
     path: '/edit-annonce/:id',
     name: 'edit-annonce',
     component: () => import('../views/annonces/EditAnnonceView.vue'),
     meta: { requiresAuth: true }
   },
-  {
-    path: '/search',
-    name: 'search',
-    component: () => import('../views/annonces/AnnonceSearchView.vue')
-  },
 
-  // --- Espace Compte (Account) ---
-  {
-    path: '/my-annonces',
-    name: 'my-annonces',
-    component: () => import('../views/account/MyAnnoncesView.vue')
-  },
-  {
-    path: '/messages',
-    name: 'messages',
-    component: () => import('../views/account/MessagesView.vue')
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/auth/LoginView.vue')
-  // --- Espace Compte ---
+  // --- Espace Compte & Profil (Protected) ---
   {
     path: '/my-annonces',
     name: 'my-annonces',
@@ -93,26 +70,16 @@ const routes = [
   {
     path: '/favorites',
     name: 'favorites',
-    component: () => import('../views/account/FavoritesView.vue')
-    path: '/favorites',
-    name: 'favorites',
     component: () => import('../views/account/FavoritesView.vue'),
     meta: { requiresAuth: true }
   },
   {
     path: '/settings',
     name: 'settings',
-    component: () => import('../views/account/AccountSettingsView.vue')
-    path: '/settings',
-    name: 'settings',
     component: () => import('../views/account/AccountSettingsView.vue'),
     meta: { requiresAuth: true }
   },
-  // --- Profil ---
   {
-    path: '/register',
-    name: 'register', 
-    component: () => import('../views/auth/RegisterView.vue')
     path: '/profil',
     name: 'profile',
     component: () => import('../views/profile/ProfileView.vue'),
@@ -121,22 +88,9 @@ const routes = [
   {
     path: '/edit-profile',
     name: 'edit-profile',
-    component: () => import('../views/profile/EditProfileView.vue')
-  },
-  {
-    path: '/profil',
-    name: 'profile',
-    component: () => import('../views/profile/ProfileView.vue')
     component: () => import('../views/profile/EditProfileView.vue'),
     meta: { requiresAuth: true }
   },
-  {
-    path: '/security',
-    name: 'security',
-    component: () => import('../views/profile/SecurityView.vue')
-  },
-
-
   {
     path: '/security',
     name: 'security',
@@ -151,13 +105,16 @@ const router = createRouter({
 })
 
 
-router.beforeEach((to, from) => {
+router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
 
   if (requiresAuth && !authState.isLoggedIn()) {
-    return { name: 'login' }; 
-  }
+   
+    next({ name: 'login' });
+  } else {
   
+    next();
+  }
 });
 
 export default router;
